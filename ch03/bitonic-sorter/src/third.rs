9bfdb923f0552e1d073250c73c1b615588c0270d
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-19 12:53:07 +0900

    Student データを氏名順にソートするテストを実装

--- ch03/bitonic-sorter/src/third.rs
+++ ch03/bitonic-sorter/src/third.rs
@@ -156,4 +156,24 @@ mod tests {
     assert_eq!(sort_by(&mut x, &|a, b| a.age.cmp(&b.age)), Ok(()));
     assert_eq!(x, expected);
   }
+
+  #[test]
+  fn sort_students_by_name_ascending() {
+    let taro = Student::new("Taro", "Yamada", 16);
+    let hanako = Student::new("Hanako", "Yamada", 14);
+    let kyoko = Student::new("Kyoko", "Ito", 15);
+    let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+    let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+    let expected = vec![&ryosuke, &kyoko, &hanako, &taro];
+
+    assert_eq!(
+      sort_by(&mut x, &|a, b| a
+        .last_name
+        .cmp(&b.last_name)
+        .then_with(|| a.first_name.cmp(&b.first_name))),
+      Ok(())
+    );
+    assert_eq!(x, expected);
+  }
 }
