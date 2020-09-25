e478d637095180f387f5e12f2f024ca9e51649d2
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-25 20:41:51 +0900

    [chapter5] box type

---
+++ chapter5/ex05/examples/ch05_01_box.rs
@@ -0,0 +1,8 @@
+fn main() {
+  let t1 = (3, "birds".to_string());
+  let mut b1 = Box::new(t1);
+  println!("{:?}", b1);
+  (*b1).0 += 1;
+  assert_eq!(*b1, (4, "birds".to_string()));
+  println!("{:?}", b1);
+}
