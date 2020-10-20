dfdd6421aa57eaf3f6cf5142c6c107d3a11a1732
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-20 20:18:53 +0900

    [chapter5] string type

---
+++ chapter5/ex05/examples/ch05_05_string1.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let mut s1 = "ラズベリー".to_string();
+  let mut s2 = String::from("ブラックベリー");
+  let s3 = "ストロベリー".to_owned();
+
+  s1.push_str("タルト");
+  s2.push('と');
+  s2.push_str(&s3);
+
+  println!("{}, {}, {}", s1, s2, s3);
+}
