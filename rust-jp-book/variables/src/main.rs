624dc5af96bc543cbc4bc754c8ab8c3534d2a386
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-27 19:49:10 +0900

    feat(rust-jp-book/variables): シャドーイング

--- rust-jp-book/variables/src/main.rs
+++ rust-jp-book/variables/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    let x2 = 5;
+    let x2 = x2 + 1;
+    let x2 = x2 * 2;
+    println!("The value of x2 is: {}", x2);
 }
