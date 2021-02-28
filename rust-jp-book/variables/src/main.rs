da118e2a007c424b6f02ae8dd0004d0797adc850
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-28 09:01:26 +0900

    feat(rust-jp-book/variables): 数値演算

--- rust-jp-book/variables/src/main.rs
+++ rust-jp-book/variables/src/main.rs
@@ -9,4 +9,11 @@ fn main() {
     println!("The value of x2 is: {}", x2);
     let guess: u32 = "42".parse().expect("Not a number");
     println!("The value of guess is: {}", guess);
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+    println!("The value of sum is: {}, difference is: {}, product is: {}, quotient is: {}, remainder is: {} ", sum, difference, product, quotient, remainder);
 }
