1cef8b9c30f44d32e9c426c1f9503fa06294c241
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-01 20:57:27 +0900

    feat(rust-jp-book/loops): while ループの使用

--- rust-jp-book/loops/src/main.rs
+++ rust-jp-book/loops/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!")
 }
