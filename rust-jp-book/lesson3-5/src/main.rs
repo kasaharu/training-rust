6009bd16f855b14393520db5f2f222c807290282
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-01 21:23:20 +0900

    feat(rust-jp-book/lesson3-5): フィボナッチ数列の n 番目を返す関数を実装

--- rust-jp-book/lesson3-5/src/main.rs
+++ rust-jp-book/lesson3-5/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let number = fibonacci(15);
+    println!("{}", number);
+}
+
+fn fibonacci(n: i32) -> i32 {
+    if n == 0 || n == 1 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
