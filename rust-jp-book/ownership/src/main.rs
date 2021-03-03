44c20d513a37adc5f3cd0ba92b84d8e32aa1b9d3
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-03 14:15:07 +0900

    feat(rust-jp-book/ownership): 返り値とスコープ

--- rust-jp-book/ownership/src/main.rs
+++ rust-jp-book/ownership/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s1 = gives_ownership();
     takes_ownership(s1);
     // println!("s1 = {}", s1); // 所有権が移動しているため s1 は有効ではない
+
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+    println!("s3 = {}", s3);
 
     let x = 10;
     makes_copy(x);
@@ -14,4 +18,14 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("some_integer = {}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
