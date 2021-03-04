a6585778eb290af8bf1656f99348259cef0e3440
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-04 09:12:20 +0900

    feat(rust-jp-book/structs): 構造体の宣言

--- rust-jp-book/structs/src/main.rs
+++ rust-jp-book/structs/src/main.rs
@@ -1,3 +1,21 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+    println!(
+        "username: {}, email: {}, sign_in_count: {}, active: {}",
+        user1.username, user1.email, user1.sign_in_count, user1.active
+    );
 }
