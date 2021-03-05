96bae0f9a8393e3308a3e886d6a8cf485cac6c9b
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-05 10:12:59 +0900

    feat(rust-jp-book/rectangles): Debug トレイトを導出する注釈を追加

--- rust-jp-book/rectangles/src/main.rs
+++ rust-jp-book/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,6 +10,7 @@ fn main() {
         height: 50,
     };
     println!("The area of rectangle is {} square pixels.", area(&rect1));
+    println!("rect1 is {:#?}.", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
