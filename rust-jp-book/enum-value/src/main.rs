59df7d191f34e30b431182385fc23689b1813278
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-07 18:39:34 +0900

    feat(rust-jp-book/enum-value): Some と None

--- rust-jp-book/enum-value/src/main.rs
+++ rust-jp-book/enum-value/src/main.rs
@@ -14,4 +14,11 @@ impl Message {
 fn main() {
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    println!("some_number is {:?}", some_number);
+    println!("some_string is {:?}", some_string);
+    println!("absent_number is {:?}", absent_number);
 }
