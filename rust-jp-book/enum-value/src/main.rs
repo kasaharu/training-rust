500045998cfd189f2a9c33463c10485afad69cdb
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-07 15:56:14 +0900

    feat(rust-jp-book/enum-value): Enum の定義

--- rust-jp-book/enum-value/src/main.rs
+++ rust-jp-book/enum-value/src/main.rs
@@ -1,3 +1,17 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("print self");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
