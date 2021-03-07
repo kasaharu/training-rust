3fe9c316e84fd05316758180404f7d56e8382ac5
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-07 18:55:13 +0900

    feat(rust-jp-book/pattern-match): Enum の列挙子をパターンにした match 式

--- rust-jp-book/pattern-match/src/main.rs
+++ rust-jp-book/pattern-match/src/main.rs
@@ -1,3 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("Quarter value is {}", value_in_cents(Coin::Quarter));
+    println!("Dime    value is {}", value_in_cents(Coin::Dime));
+    println!("Nickel  value is {}", value_in_cents(Coin::Nickel));
+    println!("Penny   value is {}", value_in_cents(Coin::Penny));
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
