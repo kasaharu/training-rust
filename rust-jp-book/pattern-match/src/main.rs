9a5af15e48d2a2d2144d7524be41a7fc0955bae4
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-08 19:42:17 +0900

    feat(rust-jp-book/pattern-match): UsState の値を保持する Quarter 列挙子

--- rust-jp-book/pattern-match/src/main.rs
+++ rust-jp-book/pattern-match/src/main.rs
@@ -1,13 +1,22 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("Quarter value is {}", value_in_cents(Coin::Quarter));
+    println!(
+        "Quarter value is {}",
+        value_in_cents(Coin::Quarter(UsState::Alaska))
+    );
     println!("Dime    value is {}", value_in_cents(Coin::Dime));
     println!("Nickel  value is {}", value_in_cents(Coin::Nickel));
     println!("Penny   value is {}", value_in_cents(Coin::Penny));
@@ -21,6 +30,9 @@ fn value_in_cents(coin: Coin) -> u32 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
