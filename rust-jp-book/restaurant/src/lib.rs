64a7f9c7c64308126ac73f5f99ef5a7dc047309c
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-09 21:56:58 +0900

    feat(rust-jp-book/restaurant): モジュール内構造体と関連関数の実装

--- rust-jp-book/restaurant/src/lib.rs
+++ rust-jp-book/restaurant/src/lib.rs
@@ -25,6 +25,20 @@ mod front_of_house {
 fn serve_order() {}
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
@@ -39,4 +53,8 @@ pub fn eat_at_restaurant() {
 
     // 相対パス
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please.", meal.toast);
 }
