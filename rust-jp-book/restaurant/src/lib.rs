841f5e79172655ef4b7951b1dee7f31f85dac213
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-09 20:15:07 +0900

    feat(rust-jp-book/restaurant): モジュールの公開と利用

--- rust-jp-book/restaurant/src/lib.rs
+++ rust-jp-book/restaurant/src/lib.rs
@@ -7,8 +7,8 @@ mod tests {
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -20,4 +20,12 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // 絶対パス
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // 相対パス
+    front_of_house::hosting::add_to_waitlist();
 }
