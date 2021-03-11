1b2af1affe3de6f908603b0f0082d494867eda59
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-11 20:37:07 +0900

    feat(rust-jp-book/vectors): ベクタの更新

--- rust-jp-book/vectors/src/main.rs
+++ rust-jp-book/vectors/src/main.rs
@@ -4,4 +4,11 @@ fn main() {
 
     let v2 = vec![1, 2, 3];
     println!("v2 is {:?}", v2);
+
+    let mut v3 = Vec::new();
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+    println!("v3 is {:?}", v3);
 }
