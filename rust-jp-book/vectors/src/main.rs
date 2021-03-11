7ba124b67a9d97f2175f05670012dd102794f238
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-11 21:15:11 +0900

    feat(rust-jp-book/vectors): Enum を定義してベクタに異なる型の値を保持

--- rust-jp-book/vectors/src/main.rs
+++ rust-jp-book/vectors/src/main.rs
@@ -24,4 +24,18 @@ fn main() {
     for i in &v5 {
         println!("{}", i);
     }
+
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("row is {:?}", row);
 }
