3362bc714e99cfe4a6058efb5749e00407af7e7a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-11 20:50:49 +0900

    feat(rust-jp-book/vectors): ベクタの読み取り

--- rust-jp-book/vectors/src/main.rs
+++ rust-jp-book/vectors/src/main.rs
@@ -11,4 +11,12 @@ fn main() {
     v3.push(7);
     v3.push(8);
     println!("v3 is {:?}", v3);
+
+    let v4 = vec![1, 2, 3, 4, 5];
+    let third1: &i32 = &v4[2];
+    let third2: Option<&i32> = v4.get(2);
+    let ten: Option<&i32> = v4.get(9);
+    println!("third1 is {:?}", third1);
+    println!("third2 is {:?}", third2);
+    println!("ten    is {:?}", ten);
 }
