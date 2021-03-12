77d37fc2cbbafae6c11e6dfb980e3d47d47aaea9
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-12 10:01:12 +0900

    feat(rust-jp-book/strings): 文字列の生成

--- rust-jp-book/strings/src/main.rs
+++ rust-jp-book/strings/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::new();
+    println!("s1 is {}", s1);
+
+    let data = "initial contents";
+    let s2 = data.to_string();
+    println!("s2 is {}", s2);
 }
