348f36a3b8a2890935d05caaccc6f3c6c7b46f2a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-12 10:02:44 +0900

    feat(rust-jp-book/strings): String::from を使った文字列の生成

--- rust-jp-book/strings/src/main.rs
+++ rust-jp-book/strings/src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     let data = "initial contents";
     let s2 = data.to_string();
     println!("s2 is {}", s2);
+
+    let s3 = String::from("initial contents");
+    println!("s3 is {}", s3);
 }
